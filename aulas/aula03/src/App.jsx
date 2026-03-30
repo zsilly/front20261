f392e00db4fd3f5d4fe67eead75cb390e680e206
Andreas S <andre.santos6@iesb.edu.br>
2026-03-30 10:53:32 -0300

    codigos aula03

---
+++ aulas/aula03/src/App.jsx
@@ -0,0 +1,49 @@
+function app(){
+  return<div>
+    <aside>
+      <header></header>
+      <img src="" alt="" />
+      <h1></h1>
+      <nav>
+        <ul>
+          <li>dashboard</li>
+          <li>notas</li>
+          <li>faltas</li>
+          <li>boletos</li>
+          <li>requerimentos</li>
+          <li>sair</li>
+        </ul>
+      </nav>
+    </aside>
+    <main>
+      <header>
+        <h1>Olá, aluno</h1>
+        <img src="" alt="" />
+        <h2></h2>
+        <section>
+          <article>
+            <h3></h3>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <h3></h3>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <h3></h3>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </article>
+        </section>
+      </header>
+    </main>
+  </div>
+}
+export default app;
